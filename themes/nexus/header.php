a425b331fba8f2ce01d56150bec97f16d6f24888
Sstobo <seanstobo@gmail.com>
2017-12-05 13:18:36 -0800

    header css

--- themes/nexus/header.php
+++ themes/nexus/header.php
@@ -31,7 +31,11 @@
 					<div class="header-logo"></div>
 					
 					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
-					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+
+					<div class="menu-content">
+						<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+					</div>
+					
 				</nav><!-- #site-navigation -->
 			</header><!-- #masthead -->
 

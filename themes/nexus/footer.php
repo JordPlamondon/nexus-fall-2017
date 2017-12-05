9641e64a51712cdd97046b8247ab457f8de4ad3f
Colin Yaremko <remta@shaw.ca>
2017-12-05 08:27:30 -0800

    Footer layout and information

--- themes/nexus/footer.php
+++ themes/nexus/footer.php
@@ -2,21 +2,64 @@
 /**
  * The template for displaying the footer.
  *
- * @package RED_Starter_Theme
+ * @package Nexus_Theme
  */
 
 ?>
 
 			</div><!-- #content -->
+<footer id="colophon" class="site-footer" role="contentinfo">
+	<div class="site-info">
+		<a href="<?php echo esc_url( 'https://nexusintercambia.com/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'Red Team' ); ?></a>
 
-			<footer id="colophon" class="site-footer" role="contentinfo">
-				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
-				</div><!-- .site-info -->
-			</footer><!-- #colophon -->
-		</div><!-- #page -->
+		<section class="bottom-flex-area">
+	
+			<div id="foot-contain" class="footer-blocks container">
+				<h3>Stay Connected</h3>
+					<div class="footer-block-item">
+						<p>
+								<span>
+									<i class="fa fa-facebook-square"></i>
+								</span>
+								<span>
+									<i class="fa fa-twitter-square"></i>
+								</span>
+								<span>
+									<i class="fa fa-youtube"></i>
+								</span>
+								<span>
+									<i class="fa fa-instagram"></i>
+								</span>
+						</p>
+						<div class="contact">
+							<h3>Contact Us</h3>
+							<p>
+								<i class="fa fa-map-marker" aria-hidden="true"></i>
+									119-970 Burrard St. Room 119 - 1st floor Vancouver, BC V6Z 2R4
+								</p>
+									<p>
+										<i class="fa fa-phone"></i>
+										<a href="tel:6042886787">604 288 6787</a>
+									</p>
 
-		<?php wp_footer(); ?>
+						</div>
+					</div>
+					<div class="footer-block-item">
+					<div class="business-hours">
+						<p class="office--hours">
+							<i class="fa fa-hourglass-end" aria-hidden="true"></i>
+							Office Hours: Monday-Friday, 9am- 5pm PST
+						</p>
+					</div>
+					</div>
+
+			</div>	
+		</section>
+	</div><!-- .site-info -->
+</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
 
 	</body>
 </html>

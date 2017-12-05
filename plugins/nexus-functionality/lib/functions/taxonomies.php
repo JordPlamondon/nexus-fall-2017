8a45b890d45ae2432f1996c82d50ecd7b4d82e86
Scott <scottdlivingstone@gmail.com>
2017-12-05 11:41:49 -0800

    Fix an error in taxonomies.php

--- plugins/nexus-functionality/lib/functions/taxonomies.php
+++ plugins/nexus-functionality/lib/functions/taxonomies.php
@@ -85,44 +85,6 @@ function nexus_tax_provinces() {
 }
 add_action( 'init', 'nexus_tax_provinces', 0 );
 
-function nexus_tax_provinces() {
-
-	$labels = array(
-		'name'                       => _x( 'Provinces', 'Taxonomy General Name', 'text_domain' ),
-		'singular_name'              => _x( 'Province', 'Taxonomy Singular Name', 'text_domain' ),
-		'menu_name'                  => __( 'Province', 'text_domain' ),
-		'all_items'                  => __( 'All Provinces', 'text_domain' ),
-		'parent_item'                => __( 'Parent Province', 'text_domain' ),
-		'parent_item_colon'          => __( 'Parent Province:', 'text_domain' ),
-		'new_item_name'              => __( 'New Province Name', 'text_domain' ),
-		'add_new_item'               => __( 'Add New Province', 'text_domain' ),
-		'edit_item'                  => __( 'Edit Province', 'text_domain' ),
-		'update_item'                => __( 'Update Province', 'text_domain' ),
-		'view_item'                  => __( 'View Province', 'text_domain' ),
-		'separate_items_with_commas' => __( 'Separate Provinces with commas', 'text_domain' ),
-		'add_or_remove_items'        => __( 'Add or remove Provinces', 'text_domain' ),
-		'choose_from_most_used'      => __( 'Choose from the most used', 'text_domain' ),
-		'popular_items'              => __( 'Popular Provinces', 'text_domain' ),
-		'search_items'               => __( 'Search Provinces', 'text_domain' ),
-		'not_found'                  => __( 'Not Found', 'text_domain' ),
-		'no_terms'                   => __( 'No Provinces', 'text_domain' ),
-		'items_list'                 => __( 'Provinces list', 'text_domain' ),
-		'items_list_navigation'      => __( 'Provinces list navigation', 'text_domain' ),
-	);
-	$args = array(
-		'labels'                     => $labels,
-		'hierarchical'               => false,
-		'public'                     => true,
-		'show_ui'                    => true,
-		'show_admin_column'          => true,
-		'show_in_nav_menus'          => true,
-		'show_tagcloud'              => true,
-		'show_in_rest'               => true,
-	);
-	register_taxonomy( 'program_type', array( 'post', ' nexus_program' ), $args );
-
-}
-add_action( 'init', 'nexus_tax_provinces', 0 );
 
 function nexus_tax_faq() {
 

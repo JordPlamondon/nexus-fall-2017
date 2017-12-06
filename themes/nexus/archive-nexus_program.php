6d56a0b12750fe55e304ac51e9e29ef2246059d3
Scott <scottdlivingstone@gmail.com>
2017-12-06 15:59:27 -0800

    continue working of api.js add a loop to display multiple posts

--- themes/nexus/archive-nexus_program.php
+++ themes/nexus/archive-nexus_program.php
@@ -14,7 +14,7 @@ get_header(); ?>
                   'taxonomy' => 'program_type',
 									'hide_empty' => 0 ) ) ) :
 			
-	    echo '<select name="programfilter"><option>Select category...</option>';
+	    echo '<select name="programfilter"><option>program</option>';
 			foreach ( $terms as $term ) :
 		    echo '<option value="' . $term->slug . '">' . $term->name . '</option>'; // ID of the category as the value of an option
 	    endforeach;
@@ -27,7 +27,7 @@ get_header(); ?>
                   'taxonomy' => 'provinces', 
 									'hide_empty' => 0) ) ) : // to make it simple I use default categories
 			echo var_dump($terms);
-	    echo '<select name="provincefilter"><option>Select category...</option>';
+	    echo '<select name="provincefilter"><option>province</option>';
 	    foreach ( $terms as $term ) :
 		    echo '<option value="' . $term->slug . '">' . $term->name . '</option>'; // ID of the category as the value of an option
 	    endforeach;
--- themes/nexus/js/api.js
+++ themes/nexus/js/api.js
@@ -9,15 +9,18 @@
         url: api_vars.root_url + request,
       })
       .done(function (data) {
-        console.log(data);
-        var post = data[0];
-        // var slug = post.slug;
-
-        // Append slug to url
-        // history.pushState({
-        //   page: slug
-        // }, null, url);
-        $('article').append(post);
+        for (var i = 0; i < data.length; i++){
+          var post = data[i];
+          var school = post["_nexus_program_school"];
+          var title = post["_nexus_program_title"];
+          // var slug = post.slug;
+          
+          // Append slug to url
+          // history.pushState({
+          //   page: slug
+          // }, null, url);
+          $('article').append(school, title);
+        }
       });
   }
 
@@ -34,8 +37,19 @@
     event.preventDefault();
     var program = $('select[name*="programfilter"').val(); 
     var province = $('select[name*="provincefilter"').val(); 
-    var request = 'wp/v2/nexus_program/?filter[program_type]=' + program + '&filter[provinces]=' + province;
-
+    var request = '';
+    if (program === 'program' && province === 'province'){
+      return;
+    }
+    else if (program === 'program'){
+      request = 'wp/v2/nexus_program/?filter[provinces]=' + province;
+    }
+    else if (province === 'province'){
+       request = 'wp/v2/nexus_program/?filter[program_type]=' + program;
+    }
+    else {
+      request = 'wp/v2/nexus_program/?filter[program_type]=' + program + '&filter[provinces]=' + province;
+    }
     console.log(program);
 
     ajaxGet(request);

5a5654d676696e9d20ec6f017989521fe2b1d66e
Jordan <jordplamondon@gmail.com>
2017-12-06 09:10:19 -0800

    add faq archive & faq single css files.

--- themes/nexus/taxonomy-nexus_faq.php
+++ themes/nexus/taxonomy-nexus_faq.php
@@ -10,6 +10,7 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 			<section class="faq-page">
+
 				<ul class "faq-categories">
 					<?php   
       			$args = array(
@@ -22,15 +23,18 @@ get_header(); ?>
 						if ($the_query->have_posts() ) : 
 						while ($the_query->have_posts() ) : $the_query->the_post()
 					?>
+
 					<li>
+						<?php the_post_thumbnail( 'full' ); ?>
 						<a href="<?php the_permalink(); ?>" rel="faq title"> 
 							<h2><?php the_title(); ?></h2>
 						</a>	
-						<?php the_post_thumbnail( 'full' ); ?>
 						<?php the_excerpt(); ?>
 					</li>
+
 					<?php endwhile; // End of the loop. ?>
 					<?php endif; ?>
+					
 				</ul>				
 			</section>				
 		</main><!-- #main -->

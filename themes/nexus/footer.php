2b7e1bacc251d8d7a933d8e24bd2dbd13aa6d10f
Colin Yaremko <remta@shaw.ca>
2017-12-06 11:16:54 -0800

    footer SCSS desktop & mobile

--- themes/nexus/footer.php
+++ themes/nexus/footer.php
@@ -12,20 +12,20 @@
 
 		<?php get_sidebar(); ?>
 	<div class="footer_hidden--container">
-	<div class="footer__site-info hidden">
-		<div class="footer_hidden_anchor">
-		<a href="<?php echo esc_url( 'https://nexusintercambia.com/' ); ?>"><?php printf( esc_html( '&copy; Copyright - Nexus Exchange %s' ), '2017' ); ?></a>
-		</div> <!--footer_hidden_anchor-->
-	<div class="footer__desktop--nav">
-			<ul class="footer_desktop--nav--ul">
-			<li><a href="https://" target="_blank">Program</a></li>
-			<li><a href="https://" target="_blank">About Us</a></li>
-			<li><a href="https://" target="_blank">FAQ</a></li>
-			<li><a href="https://" target="_blank">Contact</a></li>
-			<li><a href="https://" target="_blank">Blog</a></li>
-	</div> <!--footer__desktop--nav-->
-</div> <!--footer__site-info hidden-->
-</div> <!--footer_hidden--container-->
+		<div class="footer__site-info hidden">
+			<div class="footer_hidden_anchor">
+				<a href="<?php echo esc_url( 'https://nexusintercambia.com/' ); ?>"><?php printf( esc_html( '&copy; Copyright - Nexus Exchange %s' ), '2017' ); ?></a>
+			</div> <!--footer_hidden_anchor-->
+			<div class="footer__desktop--nav">
+				<ul class="footer_desktop--nav--ul">
+					<li class="footer--nav--li"><a href="https://" target="_blank">Program</a></li>
+					<li class="footer--nav--li"><a href="https://" target="_blank">About Us</a></li>
+					<li class="footer--nav--li"><a href="https://" target="_blank">FAQ</a></li>
+					<li class="footer--nav--li"><a href="https://" target="_blank">Contact</a></li>
+					<li class="footer--nav--li"><a href="https://" target="_blank">Blog</a></li>
+			</div> <!--footer__desktop--nav-->
+		</div> <!--footer__site-info hidden-->
+	</div> <!--footer_hidden--container-->
 
 	<?php wp_footer(); ?>
 

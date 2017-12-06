83b8ec156bd85c4d8f436abf825c235e6cbb0682
Jordan <jordplamondon@gmail.com>
2017-12-06 01:44:07 -0800

    add if loop to display faq posts on advice page.

--- themes/nexus/taxonomy-nexus_faq.php
+++ themes/nexus/taxonomy-nexus_faq.php
@@ -10,15 +10,22 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
+		<?php   
+      $args = array(
+        'post_type' => 'nexus_faq'
+			);		
+			$the_query = new WP_Query($args);
+		?>
 
-			<?php while ( have_posts() ) : the_post(); ?>
-			
+		<?php 
+			if ($the_query->have_posts() ) : while ($the_query->have_posts() ) : $the_query->the_post(); ?>
 
-
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
-
+		<h2><?php the_title(); ?></h2>
+		<?php the_post_thumbnail( 'full' ); ?>
+		<?php the_excerpt(); ?>
 
 			<?php endwhile; // End of the loop. ?>
+		<?php endif; ?>
 
 
 		</main><!-- #main -->

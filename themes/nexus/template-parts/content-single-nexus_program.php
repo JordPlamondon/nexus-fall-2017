9fbf5b550a656fe60356d2264ea23cae015b743c
Scott <scottdlivingstone@gmail.com>
2017-12-11 15:47:35 -0800

    Fix merge conflict

--- themes/nexus/template-parts/content-single-nexus_program.php
+++ themes/nexus/template-parts/content-single-nexus_program.php
@@ -32,8 +32,10 @@
         </div>
         <h2>About Program</h2>
         <p><?php echo get_post_meta($post->ID, '_nexus_program_about', true); ?></p>
+        <p><?php echo get_post_meta($post->ID, '_nexus_program_media', true); ?></p>
         <h2><?php echo get_post_meta($post->ID, '_nexus_program_school', true); ?></h2>
         <p><?php echo get_post_meta($post->ID, '_nexus_program_school_about', true); ?></p>
+        <div><?php echo get_post_meta($post->ID, '_nexus_program_city_photo', true); ?></div>
         <h2><?php echo get_post_meta($post->ID, '_nexus_program_city', true); ?></h2>
         <p><?php echo get_post_meta($post->ID, '_nexus_program_city_about', true); ?></p>
         <p><?php echo get_post_meta($post->ID, '_nexus_program_reviews', true); ?></p>

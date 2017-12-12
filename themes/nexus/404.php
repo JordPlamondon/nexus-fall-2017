f85c51ae9cb43490f3e8a1148c1d612527d8bed2
Colin Yaremko <remta@shaw.ca>
2017-12-12 09:02:12 -0800

    Rework 404 page

--- themes/nexus/404.php
+++ themes/nexus/404.php
@@ -11,22 +11,22 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+			<div class="hero-404">
+				<section class="error-404 not-found">
+					<header class="page-header">
+						<h1 class="page-title"><?php echo esc_html( 'Page Not Found' ); ?></h1>
+					</header><!-- .page-header -->
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php echo esc_html( 'Oops! That page can&rsquo;t be found.' ); ?></h1>
-				</header><!-- .page-header -->
+					<div class="page-content">
+						<p><?php echo esc_html( 'Not to worry, visit the Nexus homepage by clicking the button below' ); ?></p>
 
-				<div class="page-content">
-					<p><?php echo esc_html( 'It looks like nothing was found at this location. You can use your browser&rsquo;s back button or make a choice from the main menu.' ); ?></p>
+						<div class="return-home">
+							<a href="<?php echo home_url() ?>"><button class="home-button">Home</button></a>
+						</div>
 
-					<div class="return-home">
-						<a href="<?php echo home_url() ?>"><button class="home-button">Home</button></a>
-					</div>
-
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
+					</div><!-- .page-content -->
+				</section><!-- .error-404 -->
+			</div> <!-- hero-404 -->
 		</main><!-- #main -->
 	</div><!-- #primary -->
 

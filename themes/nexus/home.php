efdaf4ad82632a7f84de340daf0cb41dfbb5a3b0
Jordan <jordplamondon@gmail.com>
2017-12-14 01:41:11 -0800

    fix nesting problems on blog page desktop.

--- themes/nexus/home.php
+++ themes/nexus/home.php
@@ -38,14 +38,16 @@ get_header(); ?>
               </div>
               <div class="blog-text"> 
                 <h2 class="blog-title"><?php the_title(); ?></h2>
-                <div class="blog-content">
+                <div class="blog-content-button">
+                  <div class="blog-content">
                     <?php the_content(); ?>
                   </div>
                   <a href="<?php the_permalink(); ?>" rel="blog title">
-                <div style="text-align:center;" class="read-button">
-                  <button class="read-more">Read More</button>
-                </a>
-              </div>
+                  <div style="text-align:center;" class="read-button">
+                    <button class="read-more">Read More</button>
+                  </a>
+                  </div>
+                </div>  
               </div>
             </li>
             <!-- <div class="underline"></div> -->

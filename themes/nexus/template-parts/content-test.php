774bd03e457d058a03020df6c68e3057c4ed1de2
Scott <scottdlivingstone@gmail.com>
2017-12-14 09:56:15 -0800

    Add post type for front page carousel

---
+++ themes/nexus/template-parts/content-test.php
@@ -0,0 +1,37 @@
+  <?php
+  /**
+   * Template part for displaying single posts.
+   *
+   * @package nexus_Theme
+   */
+
+  ?>
+
+  <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+  	<header class="entry-header">
+  		<?php if ( has_post_thumbnail() ) : ?>
+  			<?php the_post_thumbnail( 'large' ); ?>
+  		<?php endif; ?>
+
+  		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+
+  		<div class="entry-meta">
+  			<?php nexus_posted_on(); ?> / <?php nexus_comment_count(); ?> / <?php nexus_posted_by(); ?>
+  		</div><!-- .entry-meta -->
+  	</header><!-- .entry-header -->
+
+  	<div class="entry-content">
+      <p class="single-program-content"><?php echo get_post_meta($post->ID, '_nexus_program_about', true); ?></p>
+  		<?php the_content(); ?>
+  		<?php
+  			wp_link_pages( array(
+  				'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
+  				'after'  => '</div>',
+  			) );
+  		?>
+  	</div><!-- .entry-content -->
+
+  	<footer class="entry-footer">
+  		<?php nexus_entry_footer(); ?>
+  	</footer><!-- .entry-footer -->
+  </article><!-- #post-## -->

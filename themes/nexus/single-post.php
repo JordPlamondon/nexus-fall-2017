db514489d886ed669b173b6792a9e60e1e49b7ef
Jordan <jordplamondon@gmail.com>
2017-12-15 10:11:16 -0800

    add tablet break points on faq page.

--- themes/nexus/single-post.php
+++ themes/nexus/single-post.php
@@ -13,6 +13,10 @@ get_header(); ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+		
+					
+					<?php the_title(); ?>
+
 
 			<?php endwhile; // End of the loop. ?>
 

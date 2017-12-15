9ff1ad3d7678bc3887d3d00f05d820de1a928f79
Sstobo <seanstobo@gmail.com>
2017-12-15 10:47:39 -0800

    single blog page

--- themes/nexus/single-post.php
+++ themes/nexus/single-post.php
@@ -7,14 +7,42 @@
 
 get_header(); ?>
 
+
+
+
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+			<div class="blog-wrapper">
 
-			<?php while ( have_posts() ) : the_post(); ?>
+			<div class="blog-hero">
+ <h1> Blog Posts </h1></br>
+<div class="underline"></div>
+</div>
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+
+<?php $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
+echo '<div class="blog-picture" style="background: url('. $url.')">'; ?>
+
+	<h2> <?php the_title(); ?> </h2>
+	<p>	<?php nexus_posted_on(); ?> / <?php nexus_comment_count(); ?> / <?php nexus_posted_by(); ?></p>
+	<!-- title , meta, image -->
+</div>
+<!-- dummy text -->
+	<article class="blog-post"><p> 
+	<?php while ( have_posts() ) : the_post(); ?>
+
+	<?php the_content(); ?>
 
 			<?php endwhile; // End of the loop. ?>
+</p></article>
+<!--  -->
+</div>
+
+
+
+
+	
+
 
 		</main><!-- #main -->
 	</div><!-- #primary -->

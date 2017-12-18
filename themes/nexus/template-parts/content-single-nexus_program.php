9a0b0ec78d8c1ede98d41f533d2f13aa89e3887e
Scott <scottdlivingstone@gmail.com>
2017-12-18 11:34:40 -0800

    change review to content templtate

--- themes/nexus/template-parts/content-single-nexus_program.php
+++ themes/nexus/template-parts/content-single-nexus_program.php
@@ -187,11 +187,13 @@
     $review_image = wp_get_attachment_url ( get_post_thumbnail_id($post_id), 'thumbnail' );
     ?>
     
-				<div class="headergroup">
-					<h2 class="student-review-header"> Student Review </h2>
-					<div class="underline"></div>
-                </div>
-    	<div class="student-review__box">
+		<div class="headergroup">
+			<h2 class="student-review-header"> Student Review </h2>
+			<div class="underline"></div>
+        </div>
+
+        <?php get_template_part( 'template-parts/content-review' ); ?>
+    	<!-- <div class="student-review__box">
             <div class="student-review__box__image">
                 <img src="<?php echo $review_image ?>" alt="Picture of a Student">
             </div>
@@ -202,7 +204,7 @@
                 </blockquote>
                 
             </div>
-        </div>
+        </div> -->
 <?php endif; ?>
 <?php wp_reset_query();?>
 

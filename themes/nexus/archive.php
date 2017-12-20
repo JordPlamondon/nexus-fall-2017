ae32e1ceae677ab78d2a3934a0d4dc1ff8596404
Jordan <jordplamondon@gmail.com>
2017-12-20 12:33:51 -0800

    style blog archive page.

--- themes/nexus/archive.php
+++ themes/nexus/archive.php
@@ -9,10 +9,11 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
-		<?php if ( have_posts() ) : ?>
+		<div class= "archive-page">
+			<?php if ( have_posts() ) : ?>	
 
 			<header class="page-header">
+			<h2 class="archive-main-title">Blog Archives</h2>
 				<?php
 					the_archive_title( '<h1 class="page-title">', '</h1>' );
 					the_archive_description( '<div class="taxonomy-description">', '</div>' );
@@ -32,10 +33,10 @@ get_header(); ?>
 
 		<?php else : ?>
 
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+	
 
 		<?php endif; ?>
-
+		</div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 

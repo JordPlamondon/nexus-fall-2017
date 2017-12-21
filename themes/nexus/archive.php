5ef88f41d5624f1d32fbdaecd75b34696579227d
Jordan <jordplamondon@gmail.com>
2017-12-20 16:39:21 -0800

    add trim word filter to faq page category excerpt.

--- themes/nexus/archive.php
+++ themes/nexus/archive.php
@@ -9,36 +9,32 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-		<div class= "archive-page">
-			<?php if ( have_posts() ) : ?>	
+			<div class= "archive-page">
+				<?php if ( have_posts() ) : ?>	
 
-			<header class="page-header">
-			<h2 class="archive-main-title">Blog Archives</h2>
-				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="taxonomy-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
+				<header class="page-header">
+					<h2 class="archive-main-title">Blog Archives</h2>
+						<?php
+						the_archive_title( '<h1 class="page-title">', '</h1>' );
+						the_archive_description( '<div class="taxonomy-description">', '</div>' );
+						?>
+				</header><!-- .page-header -->
 			
-			<div class= "archive-posts">
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+				<div class= "archive-posts">
+					<?php /* Start the Loop */ ?>
+					<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php
-					get_template_part( 'template-parts/content' );
-				?>
+						<?php
+							get_template_part( 'template-parts/content' );
+						?>
 
-			<?php endwhile; ?>
+					<?php endwhile; ?>
 
-			<?php the_posts_navigation(); ?>
+					<?php else : ?>
 
-		<?php else : ?>
-
-	
-
-		<?php endif; ?>
-		</div>
-		</div>
+				<?php endif; ?>
+				</div>
+			</div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
